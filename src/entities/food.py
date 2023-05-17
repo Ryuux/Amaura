79fcbd68efb3e9cb38d7b0f8ad7ff99d81bdda49
Ryuux <iloveryuux@gmail.com>
2023-05-16 23:39:14 -0400

    feat: restrocturing coding

--- src/entities/food.py
+++ src/entities/food.py
@@ -8,9 +8,9 @@ class Food:
         self.generate()
 
     def generate(self):
-        x = random.randint(0, WIDTH // BLOCK_SIZE - 1) * BLOCK_SIZE
-        y = random.randint(0, HEIGHT // BLOCK_SIZE - 1) * BLOCK_SIZE
+        x = random.randint(0, (WIDTH - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
         self.position = (x, y)
 
     def draw(self, surface):
-        pygame.draw.rect(surface, RED, (self.position[0], self.position[1], BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(surface, RED, (*self.position, BLOCK_SIZE, BLOCK_SIZE))
--- src/main.py
+++ src/main.py
@@ -3,45 +3,55 @@ from entities.snake import Snake
 from entities.food import Food
 from constants import BLACK, WIDTH, HEIGHT
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Amaura')
+        self.clock = pygame.time.Clock()
+        self.snake = Snake()
+        self.food = Food()
+        self.running = True
 
-pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Amaura')
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.snake.change_direction(pygame.K_UP)
+                elif event.key == pygame.K_DOWN:
+                    self.snake.change_direction(pygame.K_DOWN)
+                elif event.key == pygame.K_LEFT:
+                    self.snake.change_direction(pygame.K_LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    self.snake.change_direction(pygame.K_RIGHT)
 
-clock = pygame.time.Clock()
+    def update(self):
+        self.snake.move()
 
-snake = Snake()
-food = Food()
+        if self.snake.eat(self.food.position):
+            self.snake.grow()
+            self.food.generate()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                snake.change_direction(pygame.K_UP)
-            elif event.key == pygame.K_DOWN:
-                snake.change_direction(pygame.K_DOWN)
-            elif event.key == pygame.K_LEFT:
-                snake.change_direction(pygame.K_LEFT)
-            elif event.key == pygame.K_RIGHT:
-                snake.change_direction(pygame.K_RIGHT)
+        if self.snake.check_collision():
+            self.running = False
 
-    snake.move()
+    def draw(self):
+        self.window.fill(BLACK)
+        self.snake.draw(self.window)
+        self.food.draw(self.window)
+        pygame.display.flip()
 
-    if snake.eat(food.position):
-        snake.grow()
-        food.generate()
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(10)
 
-    if snake.check_collision():
-        running = False
+        pygame.quit()
 
-    window.fill(BLACK)
-    snake.draw(window)
-    food.draw(window)
-    pygame.display.flip()
-
-    clock.tick(10)  
-
-pygame.quit()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
